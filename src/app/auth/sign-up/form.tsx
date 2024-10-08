0566cbb1494f34e36e8830e3463ff8b3f87ffd99
Alex Longo <91230868+BlockadeRunner@users.noreply.github.com>
2024-10-08 17:57:11 -0400

    Added/Fixed Dark Mode

--- src/app/auth/sign-up/form.tsx
+++ src/app/auth/sign-up/form.tsx
@@ -16,7 +16,9 @@ export default function Form() {
       className="w-full flex flex-col items-center justify-center space-y-8 p-5"
     >
       {/*Register label*/}
-      <p className="mt-10 text-black font-bold w-full">Register:</p>
+      <p className="mt-10 text-black dark:text-dim-white font-bold w-full">
+        Register:
+      </p>
       {/*Username and Password Row*/}
       <div className="flex flex-row justify-center space-x-4 w-full mb-30">
         <div className="flex-1 w-full justify-center">
@@ -26,7 +28,7 @@ export default function Form() {
               type="text"
               placeholder="Enter Username:"
               required
-              className="w-full p-3 text-gray-900 text-sm bg-gray-50 border border-0 border-b-2 border-b-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-b-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="w-full p-3 text-gray-900 text-sm border-2 border-darker-blue bg-gray-50 outline-none rounded-md focus:ring-darker-blue focus:border-darker-blue dark:bg-darkest-blue dark:border-dark-mode-blue-highlight dark:focus:ring-dark-mode-blue-highlight dark:focus:border-dark-mode-blue-highlight dark:text-dark-mode-blue-highlight dark:placeholder-dark-mode-blue-highlight"
             />
           </div>
         </div>
@@ -37,12 +39,15 @@ export default function Form() {
               type="password"
               placeholder="Enter password:"
               required
-              className="w-full p-3 text-gray-900 text-sm bg-gray-50 border border-0 border-b-2 border-b-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-b-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="w-full p-3 text-gray-900 text-sm border-2 border-darker-blue bg-gray-50 outline-none rounded-md focus:ring-darker-blue focus:border-darker-blue dark:bg-darkest-blue dark:border-dark-mode-blue-highlight dark:focus:ring-dark-mode-blue-highlight dark:focus:border-dark-mode-blue-highlight dark:text-dark-mode-blue-highlight dark:placeholder-dark-mode-blue-highlight"
             />
           </div>
         </div>
       </div>
-      <p className="pt-5 text-black font-bold mt-50 w-full">Personal Info:</p>
+      {/*Personal Info Label*/}
+      <p className="pt-5 text-black dark:text-dim-white font-bold mt-50 w-full">
+        Personal Info:
+      </p>
       {/*Firstname and Lastname Row*/}
       <div className="flex flex-row justify-center space-x-4 w-full">
         <div className="flex-1 w-full justify-center">
@@ -52,7 +57,7 @@ export default function Form() {
               type="text"
               placeholder="First Name:"
               required
-              className="w-full p-3 text-gray-900 text-sm bg-gray-50 border border-0 border-b-2 border-b-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-b-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="w-full p-3 text-gray-900 text-sm border-2 border-darker-blue bg-gray-50 outline-none rounded-md focus:ring-darker-blue focus:border-darker-blue dark:bg-darkest-blue dark:border-dark-mode-blue-highlight dark:focus:ring-dark-mode-blue-highlight dark:focus:border-dark-mode-blue-highlight dark:text-dark-mode-blue-highlight dark:placeholder-dark-mode-blue-highlight"
             />
           </div>
         </div>
@@ -63,7 +68,7 @@ export default function Form() {
               type="text"
               placeholder="Last Name:"
               required
-              className="w-full p-3 text-gray-900 text-sm bg-gray-50 border border-0 border-b-2 border-b-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-b-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="w-full p-3 text-gray-900 text-sm border-2 border-darker-blue bg-gray-50 outline-none rounded-md focus:ring-darker-blue focus:border-darker-blue dark:bg-darkest-blue dark:border-dark-mode-blue-highlight dark:focus:ring-dark-mode-blue-highlight dark:focus:border-dark-mode-blue-highlight dark:text-dark-mode-blue-highlight dark:placeholder-dark-mode-blue-highlight"
             />
           </div>
         </div>
@@ -77,7 +82,7 @@ export default function Form() {
               type="email"
               placeholder="Email:"
               required
-              className="w-full p-3 text-gray-900 text-sm bg-gray-50 border border-0 border-b-2 border-b-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-b-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="w-full p-3 text-gray-900 text-sm border-2 border-darker-blue bg-gray-50 outline-none rounded-md focus:ring-darker-blue focus:border-darker-blue dark:bg-darkest-blue dark:border-dark-mode-blue-highlight dark:focus:ring-dark-mode-blue-highlight dark:focus:border-dark-mode-blue-highlight dark:text-dark-mode-blue-highlight dark:placeholder-dark-mode-blue-highlight"
             />
           </div>
         </div>
@@ -88,7 +93,7 @@ export default function Form() {
               type="tel"
               placeholder="Phone Number:"
               required
-              className="w-full p-3 text-gray-900 text-sm bg-gray-50 border border-0 border-b-2 border-b-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-b-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="w-full p-3 text-gray-900 text-sm border-2 border-darker-blue bg-gray-50 outline-none rounded-md focus:ring-darker-blue focus:border-darker-blue dark:bg-darkest-blue dark:border-dark-mode-blue-highlight dark:focus:ring-dark-mode-blue-highlight dark:focus:border-dark-mode-blue-highlight dark:text-dark-mode-blue-highlight dark:placeholder-dark-mode-blue-highlight"
             />
           </div>
         </div>
@@ -97,12 +102,12 @@ export default function Form() {
       <div className="flex flex-row justify-center space-x-4 h-100 w-full">
         <div className="flex-1 w-full h-full justify-center">
           <div className="w-full h-full">
-            <input
+            <textarea
               name="shortbio"
-              type="text"
+              rows={5}
               placeholder="Short Bio (100 words max):"
               required
-              className="w-full h-full p-3 text-gray-900 text-sm bg-gray-50 border border-0 border-b-2 border-b-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-b-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="w-full h-full resize-none p-3 text-gray-900 text-sm border-2 border-darker-blue bg-gray-50 outline-none rounded-md focus:ring-darker-blue focus:border-darker-blue dark:bg-darkest-blue dark:border-dark-mode-blue-highlight dark:focus:ring-dark-mode-blue-highlight dark:focus:border-dark-mode-blue-highlight dark:text-dark-mode-blue-highlight dark:placeholder-dark-mode-blue-highlight"
             />
           </div>
         </div>
@@ -113,7 +118,7 @@ export default function Form() {
       <div>
         <button
           type="submit"
-          className="border rounded-md p-3 bg-darker-blue border-darker-blue text-white"
+          className="border rounded-md py-3 px-16 font-bold bg-darker-blue dark:bg-dim-white border-darker-blue text-white dark:text-darker-blue"
           onClick={(event: React.MouseEvent<HTMLElement>) => {
             console.log("Signup Button Pressed");
           }}
--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -2,19 +2,20 @@ import Form from "./form";
 
 export default function SignUp() {
   return (
-    <div className="h-full w-full bg-darker-blue overscroll-none">
-      <main className="h-full w-full flex flex-col bg-darker-blue overscroll-none">
-        <h1 className="text-6xl mt-6 mb-6 text-white font-thin text-center">
+    // Background Screen
+    <div className="h-full w-full bg-darker-blue dark:bg-black overscroll-none">
+      <main className="min-h-screen h-full w-full flex flex-col mb-10 mt-6 bg-darker-blue dark:bg-black overscroll-none">
+        <h1 className="text-6xl mt-6 mb-8 text-white dark:text-dim-white font-thin text-center">
           Create Your Account
         </h1>
         <div className="flex flex-col items-center">
           {/*Main White Form Rectangle*/}
-          <div className="mb-6 bg-white rounded-xl p-6 w-4/5 md:w-2/3 lg:w-3/5">
+          <div className="mb-6 bg-white dark:bg-darker-blue rounded-xl p-6 w-4/5 md:w-2/3 lg:w-3/5">
             <Form />
             <p className="text-center">
               Already have an account?{" "}
               <a
-                href="PLACEHOLDER"
+                href="/auth/login"
                 className="text-blue-600 underline dark:text-blue-500 hover:no-underline"
               >
                 Login
