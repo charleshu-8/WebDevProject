9f479a31a3cb20983a804ac2c0e4e124783f9a11
Alex Longo <91230868+BlockadeRunner@users.noreply.github.com>
2024-10-08 14:22:43 -0400

    Adjusted width to remove horizontal scroll bar
    Also added some blue space between white rectangle and bottom of the page

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -2,14 +2,14 @@ import Form from "./form";
 
 export default function SignUp() {
   return (
-    <div className="h-full w-screen bg-darker-blue overscroll-none">
-      <main className="h-full w-screen flex flex-col bg-darker-blue overscroll-none">
+    <div className="h-full w-full bg-darker-blue overscroll-none">
+      <main className="h-full w-full flex flex-col bg-darker-blue overscroll-none">
         <h1 className="text-6xl mt-6 mb-6 text-white font-thin text-center">
           Create Your Account
         </h1>
         <div className="flex flex-col items-center">
           {/*Main White Form Rectangle*/}
-          <div className="bg-white rounded-xl p-6 w-4/5 md:w-2/3 lg:w-3/5">
+          <div className="mb-6 bg-white rounded-xl p-6 w-4/5 md:w-2/3 lg:w-3/5">
             <Form />
             <p className="text-center">
               Already have an account?{" "}
