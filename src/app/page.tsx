b8393e490687a86a89e6800f330a5e1540cff1ea
Quentin <qcwiseman@email.wm.edu>
2024-10-20 20:28:00 -0400

    initial implementation of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex flex-col bg-[#FAFCFF]  dark:bg-[#073269]">
-      <main className="text-[#030C17] font-roboto">
-        <div className="h-[150px] bg-[#FAFCFF]">
+    <div className="h-screen w-screen flex flex-col bg-[#FAFCFF]  dark:bg-gradient-to-r from-[#073269] to-black">
+      <main className="text-[#030C17] font-roboto dark:text-[#E8F1FC]">
+        <div className="h-[150px]">
           <div className="absolute right-0 z-10">
-            <svg xmlns="http://www.w3.org/2000/svg" width="883" height="150" viewBox="0 0 883 150" fill="none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="750" height="150" viewBox="0 0 750 150" fill="none">
               <path d="M882.5 156H0L150.5 0H882.5V156Z" fill="#073269"/>
             </svg>
           </div>
@@ -17,27 +17,19 @@ export default function Home() {
               Team Slackers
             </h1>
           </div>
-          <div className="z-20 absolute top-[66px] right-[300px] font-bold text-[#E8F1FC]">
-            <Link href="/auth/password-recovery">Forgot Password</Link>
-          </div>
           <div className="z-20 absolute top-[66px] right-[200px] font-bold text-[#E8F1FC]">
-            <Link href={""}>Sign Up</Link>
+            <Link href={"auth/sign-up"}>Sign Up</Link>
           </div>
           <div className="z-20 absolute top-[66px] right-[100px] font-bold text-[#E8F1FC]">
             <Link href={"/auth/login"}>Login</Link>
           </div>
         </div>
-        <div className="relative ml-[150px]">
+        <div className="relative ml-[150px] dark:text-[#E8F1FC]">
           <h1 className="text-5xl mt-[200px]">
             Bring Order to Meetings
           </h1>
           <h2 className="mt-[25px] w-1/2 font-">
             A web-based platform that adapts Robert's Rules of Order for digital use
-          </h2>
-        </div>
-        <div className="absolute bottom-[30px] right-[30px]">
-          <h2>
-            Get Started
           </h2>
         </div>
       </main>
