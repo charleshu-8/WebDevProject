86dd77b6b3f3d5d8e097ff0a5d756b03d423a244
dpbuyrn <dpbuyrn@wm.edu>
2024-10-25 16:17:06 -0400

    Time inputs made and formatted

--- src/app/chat/make-committee/form.tsx
+++ src/app/chat/make-committee/form.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { text } from "stream/consumers";
-import { JsxElement } from "typescript";
-
 // Form component for making a committee
 export default function Form() {
   // Handle committee creation
   // TODO: Add functionality when creating committee
   async function handleResponse(response: FormData) {
     const committeeName = response.get("committeename");
+    const startHour = response.get("hour");
+    const startMinutes = response.get("minutes");
+    const timeZone = response.get("time-zone");
+    const amPM = response.get("am-pm");
+
     console.log(committeeName);
+    console.log(startHour);
+    console.log(startMinutes);
+    console.log(timeZone);
+    console.log(amPM);
   }
 
   return (
@@ -32,7 +38,55 @@ export default function Form() {
       </div>
       {/*Start time Row*/}
       <div className="w-full">
-        <label htmlFor="time">Start time:</label>
+        <label htmlFor="time" className="w-1/3">
+          Start time:
+        </label>
+        {/*Div for Time Input Boxes*/}
+        <div className="float-right flex w-2/3 flex-row space-x-4">
+          <input
+            type="number"
+            name="hour"
+            min={1}
+            max={12}
+            className="justify-center rounded border-2 border-gray-shadow text-center align-middle"
+          />
+          <p>:</p>
+          <input
+            type="number"
+            name="minutes"
+            min={0}
+            max={59}
+            step={15}
+            className="rounded border-2 border-gray-shadow text-center"
+          />
+          <select
+            name="am-pm"
+            id="am-pm"
+            className="rounded border-2 border-gray-shadow bg-inherit px-1 text-center"
+          >
+            <option value="AM">AM</option>
+            <option value="PM">PM</option>
+          </select>
+          {/*Time zone selector for all of U.S. and some other
+            major time zones*/}
+          <select
+            name="time-zone"
+            id="time-zone"
+            className="rounded border-2 border-gray-shadow bg-inherit text-center"
+          >
+            <option value="EST">EST</option>
+            <option value="CST">CST</option>
+            <option value="MST">MST</option>
+            <option value="PST">PST</option>
+            <option value="AKST">AKST</option>
+            <option value="HST">HST</option>
+            <option value="JST">JST</option>
+            <option value="CST">CST</option>
+            <option value="IST">IST</option>
+            <option value="CET">CET</option>
+            <option value="WET">WET</option>
+          </select>
+        </div>
       </div>
       {/*Invite Member Row*/}
       <div className="flex w-full flex-row">
@@ -57,6 +111,8 @@ export default function Form() {
                 "email-to-invite",
               ) as HTMLInputElement;
 
+              console.log(email.value);
+
               const nameToAdd = email.value;
 
               const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -69,17 +125,8 @@ export default function Form() {
               // Split the email at the "@" character
               const username = nameToAdd.split("@")[0];
 
-              //email.value = "";
-
-              //TO DO Create New div for new member
-              const members = document.getElementById(
-                "current-members",
-              ) as HTMLDivElement;
-              //const newMember = document.createElement("div");
-              //const newMember = makeNewMemberItem(username, "Member");
-              const newMember = makeNewMemberItem2(username, "member");
-
-              members.append(newMember);
+              email.value = "";
+              addNewMemberItem(username, "Member");
             }}
           >
             Add member
@@ -95,6 +142,7 @@ export default function Form() {
           className="flex w-2/3 flex-row space-x-4 overflow-auto"
         >
           {/*Outer div for individual member*/}
+          {/*Creator of the */}
           <div className="flex flex-row">
             <div className="mr-2 content-center">
               <div className="h-3 w-3 rounded-full bg-darker-blue"></div>
@@ -122,35 +170,9 @@ export default function Form() {
   );
 }
 
-//Included in Button Now
-// function getUsernameFromEmail(email: string): string {
-//   // Check if the email is valid
-//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//   if (!emailRegex.test(email)) {
-//     console.error("Invalid email format.");
-//     return "error";
-//   }
+function addNewMemberItem(name: string, role: string) {
+  const members = document.getElementById("current-members") as HTMLDivElement;
 
-//   // Split the email at the "@" character
-//   const username = email.split("@")[0];
-//   return username;
-// }
-
-function makeNewMemberItem(name: string, role: string): JSX.Element {
-  return (
-    <div className="flex flex-row">
-      <div className="mr-2 content-center">
-        <div className="h-3 w-3 rounded-full bg-red-500"></div>
-      </div>
-      <div className="flex flex-col">
-        <p>{name}</p>
-        <p className="text-xs text-gray-shadow">{role}</p>
-      </div>
-    </div>
-  );
-}
-
-function makeNewMemberItem2(name: string, role: string): HTMLDivElement {
   const memberDiv = document.createElement("div");
   memberDiv.className = "flex flex-row";
 
@@ -158,7 +180,7 @@ function makeNewMemberItem2(name: string, role: string): HTMLDivElement {
   iconDiv.className = "mr-2 content-center";
 
   const icon = document.createElement("div");
-  icon.className = "bg-red-500 h-3 w-3 rounded-full";
+  icon.className = "h-3 w-3 rounded-full";
 
   const textDiv = document.createElement("div");
   textDiv.className = "flex flex-col";
@@ -170,6 +192,14 @@ function makeNewMemberItem2(name: string, role: string): HTMLDivElement {
   roleDiv.className = "text-xs text-gray-shadow";
   roleDiv.innerText = role;
 
+  //Icon color should be red for members
+  //darker-blue for the owner
+  if ((role = "Member")) {
+    icon.className = icon.className + " bg-red-500";
+  } else if ((role = "Owner")) {
+    icon.className = icon.className + " bg-darker-blue";
+  }
+
   textDiv.appendChild(memberName);
   textDiv.appendChild(roleDiv);
 
@@ -178,5 +208,5 @@ function makeNewMemberItem2(name: string, role: string): HTMLDivElement {
   memberDiv.appendChild(iconDiv);
   memberDiv.appendChild(textDiv);
 
-  return memberDiv;
+  members.append(memberDiv);
 }
