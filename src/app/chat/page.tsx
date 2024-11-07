dc082a0389a4c4c30d74b07311be3b1713f80b97
Charles Hu <computerscience@verizon.net>
2024-11-07 14:31:29 -0500

    Remove boxes
    Signed-off-by: Charles Hu <computerscience@verizon.net>

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,17 +2,16 @@
 
 import SideBar from "./navbar";
 import NavBar from "./sidebar";
-import { Box } from "@mui/material";
 import ChatBox from "./chat_boxes";
 
 export default function ChatPage() {
   return (
-    <Box className="flex h-screen w-screen bg-light-background dark:bg-dark-secondary">
+    <div className="flex h-screen w-screen bg-light-background dark:bg-dark-secondary">
       <NavBar />
-      <Box className="relative top-[80px] flex h-[calc(100%-80px)] w-full flex-row">
+      <div className="relative top-[80px] flex h-[calc(100%-80px)] w-full flex-row">
         <SideBar />
         <ChatBox />
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 }
