894a54f8cd4d2b6a13837ec91343a1e74d09093c
Charles Hu <computerscience@verizon.net>
2024-11-07 14:25:41 -0500

    Chat page overlap fix
    Signed-off-by: Charles Hu <computerscience@verizon.net>

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,18 +1,18 @@
 "use client";
+
 import SideBar from "./navbar";
 import NavBar from "./sidebar";
 import { Box } from "@mui/material";
 import ChatBox from "./chat_boxes";
 
 export default function ChatPage() {
-
   return (
-    <Box className="h-screen w-screen bg-light-background">
+    <Box className="flex h-screen w-screen bg-light-background dark:bg-dark-secondary">
       <NavBar />
-      <SideBar />
-      <div className="fixed left-[70px] top-[80px] flex h-[89%] w-[93%]">
+      <Box className="relative top-[80px] flex h-[calc(100%-80px)] w-full flex-row">
+        <SideBar />
         <ChatBox />
-      </div>
+      </Box>
     </Box>
   );
 }
