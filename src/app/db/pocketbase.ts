3f5fe1999f12a4999310dfb8f919750df46cc5b6
Charles Hu <computerscience@verizon.net>
2024-11-14 18:06:29 -0500

    Linting
    Signed-off-by: Charles Hu <computerscience@verizon.net>

--- src/app/db/pocketbase.ts
+++ src/app/db/pocketbase.ts
@@ -3,8 +3,8 @@ import PocketBase from "pocketbase";
 export const pb = new PocketBase("https://slackers.pockethost.io");
 
 export let currentUser = pb.authStore.model;
-export let currentCommittee = "91ttrau140qhgdc"; //placeholder, missing logic
-export let currentMotion = "esco3pacuipvqre"; //placeholder, missing logic
+export const currentCommittee = "91ttrau140qhgdc"; //placeholder, missing logic
+export const currentMotion = "esco3pacuipvqre"; //placeholder, missing logic
 
 pb.authStore.onChange(() => {
   console.log("User changed");
